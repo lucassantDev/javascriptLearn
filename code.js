d1d84d508cce8db32c5d31175485cec599145217
Lucas Sant <lucasfdesantana095@gmail.com>
2025-01-20 21:01:59 -0300

    codigos na pagina js - 2025-01-20

---
+++ code.js
@@ -0,0 +1,15 @@
+// Tudo que estiver dentro de aspas será considerado string(textos)
+console.log("Olá mundo!");
+console.log("1 + 1 = 2");
+console.log("2025-01-20");
+
+
+// Number no javascript podem ser tanto inteiros, quanto floats(quebrados).
+console.log(1);
+console.log(1.1); // importante frizar que não se usa vírgula para informar um número float, e sim ponto
+console.log(0.2);
+
+
+// Booleanos retorna uma resposta verdadeira(true) ou falsa(false).Serve para ser usado em funções e estruturas de repetição por exemplo.
+console.log(true);
+console.log(false);
