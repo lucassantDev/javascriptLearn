701f02d2322648b2b91818a32d69db2eea8662a2
Lucas Sant <lucasfdesantana095@gmail.com>
2025-02-03 19:58:28 -0300

    adicionando pasta - 2025-01-03

---
+++ js/variaveis.js
@@ -0,0 +1,19 @@
+var nomeUsuario = "Lucas";
+console.log("Olá ", nomeUsuario);
+
+
+// é possível definir 2 variáveis na mesma linha ou abaixo da outra
+var cidade = "Recife", estado = "Pernambuco";
+console.log(estado + ", " + cidade);
+
+/*
+
+var e let são variáveis que podem ter seus valores atribuidos alterados.
+const, por outro lado, permanece com o mesmo valor atribuido, mesmo tentado muda-lo
+
+*/
+
+var time ='Barcelona';
+time = 'Real Madrid' 
+// em um primeiro momento, a variavel time tinha "Barcelona" como valor atribuido! Porém, ao informar um novo valor, a variavel time agora recebe o valor "Real Madrid".
+console.log(time)
