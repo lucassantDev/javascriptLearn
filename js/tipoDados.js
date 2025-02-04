8c0ef5b157dae011596fe52c0e67c6c59e651361
Lucas Sant <lucasfdesantana095@gmail.com>
2025-02-03 22:56:25 -0300

    tipo de dados - 2025-01-03

---
+++ js/tipoDados.js
@@ -0,0 +1,5 @@
+var nome = "Lucas";
+console.log(typeof nome);
+
+// typeof é um meio de identificar o tipo de dado atribuido a variavel
+// Pode vir a ser importante na hora de criar uma função para identificar se o dado informado é válido ou não
